4221ab493da64f6d20daef09a3ae61246b3ce6ad
yarrou <konic2006@yandex.ru>
2022-01-31 06:39:39 +0100

    MyRequestContextListener : using the class method, we get the URL of the site

---
+++ src/main/java/site/alexkononsol/siteToOK/util/MyRequestContextListener.java
@@ -0,0 +1,19 @@
+package site.alexkononsol.siteToOK.util;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.RequestContextListener;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+import javax.servlet.annotation.WebListener;
+import javax.servlet.http.HttpServletRequest;
+
+@WebListener
+@Component
+public class MyRequestContextListener extends RequestContextListener {
+    //the method is designed to get the URL of the current request
+    public String getMyUrl(){
+        HttpServletRequest request = ((ServletRequestAttributes) RequestContextHolder.currentRequestAttributes()).getRequest();
+        return request.getRequestURL().toString();
+    }
+}
