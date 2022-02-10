1f207cbac80add5d1b8b4214b9436b13fa14a831
yarrou <konic2006@yandex.ru>
2022-02-10 14:36:30 +0100

    MessagesController : reworking the code, rendering logic in MessageService

--- src/main/java/site/alexkononsol/siteToOK/controllers/MessagesController.java
+++ src/main/java/site/alexkononsol/siteToOK/controllers/MessagesController.java
@@ -1,6 +1,5 @@
 package site.alexkononsol.siteToOK.controllers;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,48 +7,39 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import site.alexkononsol.siteToOK.entity.Message;
-import site.alexkononsol.siteToOK.entity.User;
-import site.alexkononsol.siteToOK.repositories.MessageRepository;
-import site.alexkononsol.siteToOK.repositories.UserRepository;
-import site.alexkononsol.siteToOK.service.impl.UserServiceImpl;
+import site.alexkononsol.siteToOK.service.MessageService;
 
 import java.security.Principal;
 import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.LinkedHashSet;
+import java.util.List;
+import java.util.Set;
 
 @Controller
 public class MessagesController {
-    @Autowired
-    UserRepository userRepository;
-    @Autowired
-    MessageRepository messageRepository;
-    @Autowired
-    UserServiceImpl service;
+    private final MessageService messageService;
+
+    public MessagesController(MessageService messageService) {
+        this.messageService = messageService;
+    }
+
 
     @GetMapping("/messages")
     public String showMessagesPage(Principal principal, ModelMap model){
-        User user = userRepository.findByUsername(principal.getName());
-        LinkedHashSet<String> whoWroteNames = messageRepository.dialogues(principal.getName());
+        Set<String> whoWroteNames = messageService.getDialogues(principal.getName());
         model.addAttribute("whoWrote",whoWroteNames);
         return  "messages";
     }
 
     @GetMapping("/messages/{name}")
     public String readMessagesPage(@PathVariable("name") String name, ModelMap model,Principal principal){
-        model.addAttribute("messages",getMessagesList(principal.getName(),name));
+        model.addAttribute("messages",messageService.getMessagesList(principal.getName(),name));
         model.addAttribute("nameUser",name);
         return "messagesFromUser";
     }
 
     @GetMapping("/messages/{name}/sendMessage")
     public String SendMessagePage(@PathVariable("name")String name,ModelMap model,Principal principal){
-        ArrayList<Message> lastFiveMessages = new ArrayList<>();
-        Iterator<Message> iterator = getMessagesList(principal.getName(),name).iterator();
-        for(int i = 0;( i < 5 )&& iterator.hasNext();i++){
-            lastFiveMessages.add( iterator.next());
-        }
+        List<Message> lastFiveMessages = messageService.getLast5Messages(principal.getName(),name);
         Message message = new Message();
         model.addAttribute("recipient",name);
         model.addAttribute("message",message);
@@ -62,17 +52,7 @@ public class MessagesController {
         message.setRecipient(recipientName);
         message.setSender(principal.getName());
         message.setDate(LocalDateTime.now());
-        messageRepository.save(message);
+        messageService.save(message);
         return "redirect:/messages/"+recipientName;
-    }
-    ArrayList<Message> getMessagesList(String user1,String user2){
-        ArrayList<Message> messages = null;
-        if(user2.equals("administration")){
-            messages = messageRepository.requestsToAdministration(user1);
-        }
-        else {
-            messages = messageRepository.corespondence(user1,user2);
-        }
-        return messages;
     }
 }
