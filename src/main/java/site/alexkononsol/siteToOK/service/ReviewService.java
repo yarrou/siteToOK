86260b0503e3a8649ca7a8758550bbfff1fb9a53
yarrou <konic2006@yandex.ru>
2022-02-10 21:57:12 +0100

    ReviewService : service and its implementation for working with Review objects

---
+++ src/main/java/site/alexkononsol/siteToOK/service/ReviewService.java
@@ -0,0 +1,13 @@
+package site.alexkononsol.siteToOK.service;
+
+import site.alexkononsol.siteToOK.entity.Review;
+
+import java.util.List;
+
+public interface ReviewService {
+    int sizeTableForCheck();
+    List<Review> lastReviewsForCheck(int page);
+    Review getById(Long id);
+    void save(Review review);
+    void delete(Review review);
+}
