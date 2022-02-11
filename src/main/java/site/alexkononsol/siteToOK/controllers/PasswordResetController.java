8f7b415a197863b78c1447f65bdccbc79075921f
yarrou <konic2006@yandex.ru>
2022-02-11 22:06:18 +0100

    PasswordResetController : replacing the repository with a service, optimizing the code

--- src/main/java/site/alexkononsol/siteToOK/controllers/PasswordResetController.java
+++ src/main/java/site/alexkononsol/siteToOK/controllers/PasswordResetController.java
@@ -1,7 +1,5 @@
 package site.alexkononsol.siteToOK.controllers;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.ui.Model;
@@ -9,10 +7,9 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import site.alexkononsol.siteToOK.entity.PasswordResetToken;
-import site.alexkononsol.siteToOK.entity.User;
 import site.alexkononsol.siteToOK.form.PasswordResetForm;
-import site.alexkononsol.siteToOK.repositories.PasswordResetTokenRepository;
-import site.alexkononsol.siteToOK.service.impl.UserServiceImpl;
+import site.alexkononsol.siteToOK.service.PasswordResetTokenService;
+import site.alexkononsol.siteToOK.service.UserService;
 
 import javax.validation.Valid;
 
@@ -20,12 +17,14 @@ import javax.validation.Valid;
 @RequestMapping("/reset-password")
 public class PasswordResetController {
 
-    @Autowired
-    private UserServiceImpl userServiceImpl;
-    @Autowired
-    private PasswordResetTokenRepository tokenRepository;
-    @Autowired
-    private BCryptPasswordEncoder passwordEncoder;
+    private final UserService userService;
+    private final PasswordResetTokenService tokenService;
+
+    public PasswordResetController(UserService userService, PasswordResetTokenService tokenService) {
+        this.userService = userService;
+        this.tokenService = tokenService;
+    }
+
 
     @ModelAttribute("passwordResetForm")
     public PasswordResetForm passwordReset() {
@@ -36,7 +35,7 @@ public class PasswordResetController {
     public String displayResetPasswordPage(@RequestParam(required = false) String token,
                                            Model model) {
 
-        PasswordResetToken resetToken = tokenRepository.findByToken(token);
+        PasswordResetToken resetToken = tokenService.getByToken(token);
         if (resetToken == null){
             model.addAttribute("error", "Could not find password reset token.");
         } else if (resetToken.isExpired()){
@@ -44,7 +43,6 @@ public class PasswordResetController {
         } else {
             model.addAttribute("token", resetToken.getToken());
         }
-
         return "reset-password";
     }
 
@@ -53,20 +51,12 @@ public class PasswordResetController {
     public String handlePasswordReset(@ModelAttribute("passwordResetForm") @Valid PasswordResetForm form,
                                       BindingResult result,
                                       RedirectAttributes redirectAttributes) {
-
         if (result.hasErrors()){
             redirectAttributes.addFlashAttribute(BindingResult.class.getName() + ".passwordResetForm", result);
             redirectAttributes.addFlashAttribute("passwordResetForm", form);
             return "redirect:/reset-password?token=" + form.getToken();
         }
-
-        PasswordResetToken token = tokenRepository.findByToken(form.getToken());
-        User user = token.getUser();
-        String updatedPassword = passwordEncoder.encode(form.getPassword());
-        userServiceImpl.updatePassword(updatedPassword, user.getId());
-        tokenRepository.delete(token);
-
+        userService.upgradePassword(form.getToken(),form.getPassword());
         return "redirect:/login?resetSuccess";
     }
-
 }
