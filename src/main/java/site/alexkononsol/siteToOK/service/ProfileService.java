c4b8bdd215082126044d6a33b4c66ad57ce964f2
yarrou <konic2006@yandex.ru>
2022-02-12 08:52:54 +0100

    ProfileService : adding a method to save the state of the Profile object

--- src/main/java/site/alexkononsol/siteToOK/service/ProfileService.java
+++ src/main/java/site/alexkononsol/siteToOK/service/ProfileService.java
@@ -1,5 +1,8 @@
 package site.alexkononsol.siteToOK.service;
 
+import site.alexkononsol.siteToOK.entity.Profile;
+
 public interface ProfileService {
     byte[] getProfileImageById(Long id);
+    void save(Profile profile);
 }
