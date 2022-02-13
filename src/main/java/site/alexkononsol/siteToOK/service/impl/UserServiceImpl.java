13d9177e32f3a0a1084be86b55ee0b8f5219437c
yarrou <konic2006@yandex.ru>
2022-02-13 09:42:09 +0100

    UserServiceImpl : code optimization

--- src/main/java/site/alexkononsol/siteToOK/service/impl/UserServiceImpl.java
+++ src/main/java/site/alexkononsol/siteToOK/service/impl/UserServiceImpl.java
@@ -2,7 +2,6 @@ package site.alexkononsol.siteToOK.service.impl;
 
 import lombok.extern.slf4j.Slf4j;
 import net.bytebuddy.utility.RandomString;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -24,22 +23,31 @@ import java.util.*;
 @Slf4j
 @Service
 public class UserServiceImpl implements UserService, UserDetailsService {
-    @Autowired
-    private  PasswordResetTokenRepository passwordResetTokenRepository;
-    @Autowired
-    private  UserRepository userRepository;
-    @Autowired
-    private  RoleRepository roleRepository;
-    @Autowired
-    private  BCryptPasswordEncoder encoder;
-    @Autowired
-    private  EmailSenderService emailSenderService;
-    @Value("${S_EMAIL}")
-    private  String defaultEmail;
-    @Value("${ADMIN_NAME}")
-    private  String adminName;
-    @Value("${ADMIN_PASSWORD}")
-    private  String adminPassword;
+    private final PasswordResetTokenRepository passwordResetTokenRepository;
+    private final UserRepository userRepository;
+    private final RoleRepository roleRepository;
+    private final BCryptPasswordEncoder encoder;
+    private final EmailSenderService emailSenderService;
+    private final String defaultEmail;
+    private final String adminName;
+    private final String adminPassword;
+
+    public UserServiceImpl(PasswordResetTokenRepository passwordResetTokenRepository,
+                           UserRepository userRepository,
+                           RoleRepository roleRepository,
+                           EmailSenderService emailSenderService,
+                           @Value("${S_EMAIL}") String defaultEmail,
+                           @Value("${ADMIN_NAME}") String adminName,
+                           @Value("${ADMIN_PASSWORD}") String adminPassword) {
+        this.passwordResetTokenRepository = passwordResetTokenRepository;
+        this.userRepository = userRepository;
+        this.roleRepository = roleRepository;
+        this.encoder = new BCryptPasswordEncoder();
+        this.emailSenderService = emailSenderService;
+        this.defaultEmail = defaultEmail;
+        this.adminName = adminName;
+        this.adminPassword = adminPassword;
+    }
 
     @Override
     public void register(User user) {
